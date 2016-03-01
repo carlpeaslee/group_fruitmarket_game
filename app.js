580aeffc4754a001e4319eb31d728cefbc93ae1d
Carl Peaslee <cpeaslee@gmail.com>
2016-03-01 10:56:12 -0600

    adding correctly updating fruit prices

--- app.js
+++ app.js
@@ -7,22 +7,41 @@ function randomNumber(min, max){
 //the market updates and the interval at which it  updates
 setInterval(updateMarket, 15000);
 
-//this gives us a random number between -.5 and .5
-
-
 var marketApplePrice = 1;
 var marketBananaPrice = 1;
 var marketGrapePrice = 1;
 var marketOrangePrice = 1;
 var marketPearPrice = 1;
 
+//this is a function that updates a single fruit market price
+function singleFruitMarketUpdater(fruitMarket){
+  fruitMarket += randomNumber(-50, 50)/100;
+  if (fruitMarket < .5) {
+    fruitMarket = .5;
+  };
+
+  if (fruitMarket > 9.99) {
+    fruitMarket = 9.99;
+  };
+  return ((Math.round(fruitMarket*100))/100);
+}
+
+
 //this is the function that runs when the market updates
 function updateMarket() {
-  marketApplePrice += randomNumber(-50, 50)/100;
-  marketBananaPrice += randomNumber(-50, 50)/100;
-  marketGrapePrice += randomNumber(-50, 50)/100;
-  marketOrangePrice += randomNumber(-50, 50)/100;
-  marketPearPrice += randomNumber(-50, 50)/100;
+  //this part changes the market price of each fruit
+  marketApplePrice = singleFruitMarketUpdater(marketApplePrice);
+  $('.apple-price').text(marketApplePrice);
+  marketBananaPrice = singleFruitMarketUpdater(marketBananaPrice);
+  $('.banana-price').text(marketBananaPrice);
+  marketGrapePrice = singleFruitMarketUpdater(marketGrapePrice);
+  $('.grape-price').text(marketGrapePrice);
+  marketOrangePrice = singleFruitMarketUpdater(marketOrangePrice);
+  $('.orange-price').text(marketOrangePrice);
+  marketPearPrice = singleFruitMarketUpdater(marketPearPrice);
+  $('.pear-price').text(marketPearPrice);
+
+  console.log(marketApplePrice, marketBananaPrice, marketGrapePrice, marketOrangePrice, marketPearPrice)
 
 
 }
